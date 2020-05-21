5246cdd5d079ad03697df977c7d700411868641a
Paul Reyes <paulfermoreyes@gmail.com>
2020-05-21 12:15:03 +0800

    Added static site for Storybook

---
+++ storybook-static/precache-manifest.df654157a9d58716e1cf39edfe116cc7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7b9792306e7402a7b915dcdcc9649e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.409f13414328545553b5.bundle.js"
+  },
+  {
+    "url": "runtime~main.409f13414328545553b5.bundle.js"
+  },
+  {
+    "url": "vendors~main.409f13414328545553b5.bundle.js"
+  },
+  {
+    "url": "vendors~main.409f13414328545553b5.bundle.js.LICENSE.txt"
+  }
+]);
